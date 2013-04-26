31084b9603e12f081f57760eed9b9681e009e19b
John Ware <treymcfly22595@gmail.com>
2013-04-26 13:51:34 -0500

    created SocalDeck project and so far we have create status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialDeck2::Application.config.secret_token = 'e4abddec5093fb4894f2dcc0f791d2b587fd54c2f52110f4dc369644ffa6d531b605e2af1cc10a1b7ab1ab426e84db0836f8497f04f6bfd4c3c93e6ef68fe789'
